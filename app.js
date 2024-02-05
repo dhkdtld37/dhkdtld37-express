ba8840f6ca09821c1c3240682d94139da613dcd8
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-05 12:58:28 +0900

    Route parameters

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
 
+const router = express.Router();
+
+router.get('/artists/:id', function(req, res) {
+        console.log("id는 " + req.params.id + " 입니다.")
+        res.send("id : " + req.params.id)
+});
+
+router.get('/artists/:id/company/:company', function(req, res) {
+        res.send("id : " + req.params.id + " 회사 : " + req.params.company)
+});
+
